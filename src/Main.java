931efe2cacd81e03dae49e0953a5b68f10170de8
Vladi0404 <moskva.vladi@yandex.ru>
2023-04-11 20:12:59 +0200

    One Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // task 1
+
+        System.out.println("Задача 1");
+        int [] simply = new int [3];
+        simply[0] = 1;
+        int q = simply[0];
+        simply[1] = 2;
+        int w = simply[1];
+        simply[2] = 3;
+        int e = simply[2];
+
+        System.out.println("массив 1 = " + q + ", массив 2 = " + w + " массив 3 = " + e);
+
+        double [] simply2 = { 1.57, 7.654, 9.986};
+
+            System.out.println(simply2);
+
+
+
+        int [] simply3 = { 37, 46, 58};
+
     }
 }
