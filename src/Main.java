30499447abf48873b814b5a16626e7fd5fe60001
Vladi0404 <moskva.vladi@yandex.ru>
2023-04-12 10:35:45 +0200

    Готовое Д/З

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        // task 1
+        // task 1 - 2
 
-        System.out.println("Задача 1");
+        System.out.println("Задача 1 - 2");
         int [] simply = new int [3];
         simply[0] = 1;
         int q = simply[0];
@@ -16,11 +16,46 @@ public class Main {
 
         double [] simply2 = { 1.57, 7.654, 9.986};
 
-            System.out.println(simply2);
+        System.out.print(simply2 [0]);
+        System.out.print(", ");
+        System.out.print(simply2 [1]);
+        System.out.print(", ");
+        System.out.print(simply2 [2]);
+
+        // task 3
+
+        System.out.println(" ");
+        System.out.println("Задача 3");
+        int [] simply4 = new int [3];
+        simply4[0] = 1;
+        int a = simply4[0];
+        simply4[1] = 2;
+        int s = simply4[1];
+        simply4[2] = 3;
+        int d = simply4[2];
+
+        System.out.println("массив 3 = " + d + ", массив 2 = " + s + " массив 1 = " + a);
+
+        double [] simply5 = { 1.57, 7.654, 9.986};
+
+        System.out.print(simply2 [2]);
+        System.out.print(", ");
+        System.out.print(simply2 [1]);
+        System.out.print(", ");
+        System.out.print(simply2 [0]);
+
+        // task 4
+
+        System.out.println("Задача 4");
+        int[] simply6 = {1, 2, 3};
+        for (int i = 1; i <= simply6.length; i = i + 1) {
+            if (i <= simply6.length + 1) {
+                System.out.println(simply6[i]);
+                break;
+
+            }
 
 
-
-        int [] simply3 = { 37, 46, 58};
-
+        }
     }
 }
